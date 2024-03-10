100c31a23a7cf7fff343c4c179743fa311bdad9b
Nolan J Tait <nolanjtait@gmail.com>
2024-03-10 14:24:04 -0700

    Adds public folder with favicon and SEO

--- app/layouts/site.rb
+++ app/layouts/site.rb
@@ -3,10 +3,37 @@
 module Layouts
   class Site < Layout
     def template(&block)
-      html lang: "en-US", data: { theme: "onedark" } do
+      html lang: "en_US", data: { theme: "onedark" } do
         head do
-          title { "Protos" }
-          meta name: "viewport", content: "width=device-width,initial-scale=1"
+          title { "Protos UI components" }
+          meta property: "og:title", content: "Protos UI components"
+          meta property: "og:type", content: "website"
+          meta property: "og:locale", content: "en_US"
+          meta name: "description",
+               content: "Protos is a UI component library built with Phlex, " \
+                        "Tailwindcss, and daisyUI."
+          link rel: "canonical", href: "https://protos.inhouse.work"
+          meta property: "og:url", content: "https://protos.inhouse.work"
+          meta property: "og:site_name", content: "Protos"
+          meta name: "twitter:card", content: "summary"
+          meta property: "twitter:title", content: "Protos UI components"
+          meta name: "twitter:site", content: "@nolantait"
+          meta name: "twitter:creator", content: "@nolantait"
+
+          meta name: "viewport",
+               content: "width=device-width,initial-scale=1,viewport-fit=cover"
+          meta name: "turbo-cache-control", content: "no-preview"
+          meta name: "theme-color", content: "#61afef"
+          meta name: "mobile-web-app-capable", content: "yes"
+          meta name: "apple-touch-fullscreen", content: "yes"
+          meta name: "apple-mobile-web-app-capable", content: "yes"
+          meta name: "apple-mobile-web-app-status-bar-style", content: "default"
+          meta name: "apple-mobile-web-app-title", content: "Protos"
+          script(
+            src: "https://cdn.usefathom.com/script.js",
+            data: { site: "LODSSLWQ", turbo_track: "reload" },
+            defer: true
+          )
           vite_client_tag unless ENV["RACK_ENV"] == "production"
           vite_javascript_tag "application"
         end
--- lib/builder.rb
+++ lib/builder.rb
@@ -3,6 +3,10 @@
 class Builder
   class << self
     attr_accessor :loader
+
+    def call(...)
+      new(...).call
+    end
   end
 
   def initialize(files = Dry::Files.new)
@@ -13,6 +17,21 @@ class Builder
     compile("build/index.html") { Pages::Home.new.call }
     compile("build/404.html") { Pages::NotFound.new.call }
     compile("build/500.html") { Pages::ServiceError.new.call }
+    copy("public/robots.txt", "build/robots.txt")
+    copy("public/sitemap.xml", "build/sitemap.xml")
+    copy(
+      "public/android-chrome-192x192.png",
+      "build/android-chrome-192x192.png"
+    )
+    copy(
+      "public/android-chrome-512x512.png",
+      "build/android-chrome-512x512.png"
+    )
+    copy("public/apple-touch-icon.png", "build/apple-touch-icon.png")
+    copy("public/favicon-16x16.png", "build/favicon-16x16.png")
+    copy("public/favicon-32x32.png", "build/favicon-32x32.png")
+    copy("public/favicon.ico", "build/favicon.ico")
+    copy("public/site.webmanifest", "build/site.webmanifest")
   end
 
   def watch
@@ -35,5 +54,9 @@ class Builder
   def compile(filepath)
     content = yield
     @files.write(filepath, content)
+  end
+
+  def copy(source, destination)
+    @files.cp(source, destination)
   end
 end
