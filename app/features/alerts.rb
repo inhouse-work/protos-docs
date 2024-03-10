6a1f77f87514e885f76c05d10107c46e6d54157e
Nolan J Tait <nolanjtait@gmail.com>
2024-03-10 16:41:00 -0700

    Fixes drawers z-index with sidebar and updates alerts

--- app/features/alerts.rb
+++ app/features/alerts.rb
@@ -3,8 +3,18 @@
 module Features
   class Alerts < Component
     def template
-      render Protos::Alert.new do |_alert|
-        "Hello world"
+      render Protos::Alert.new do |alert|
+        render alert.icon do
+          icon("check-circle")
+        end
+
+        plain "Hello world"
+
+        render alert.actions do
+          button(class: "btn btn-primary btn-sm") do
+            icon("x-circle")
+          end
+        end
       end
     end
   end
--- app/features/drawers.rb
+++ app/features/drawers.rb
@@ -8,7 +8,7 @@ module Features
           render drawer.trigger(class: "btn btn-primary") { "Open Drawer" }
         end
 
-        render drawer.side do
+        render drawer.side(class: "z-[999]") do
           ul(class: "menu p-4 w-80 min-h-full bg-base-200 text-base-content") do
             li do
               a { "Main Menu" }
