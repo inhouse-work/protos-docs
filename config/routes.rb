a3b8da254ae0f620847a2c2b4a79ab455106c7b3
Nolan J Tait <nolanjtait@gmail.com>
2025-01-12 22:33:16 -0800

    Fixes dead route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Staticky.router.define do
   root to: Pages::Home
   match "404", to: Errors::NotFound
   match "500", to: Errors::ServiceError
-
-  match "docs/buttons", to: Pages::Docs::Buttons
 end
